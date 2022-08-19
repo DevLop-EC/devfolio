fb2c7e743e68416c101b0a5be5c0c06bc1731a03
DevLop-EC <christlopbsc12@gmail.com>
2022-08-19 15:28:33 -0500

    Migrate React to Next with 18.2.0

--- src/components/navbar/menu-items.js
+++
@@ -1,6 +0,0 @@
-export const MenuItems = [
-  { title: 'About', url: '#about', cName: 'nav-links' },
-  { title: 'Experience', url: '#experience', cName: 'nav-links' },
-  { title: 'Projects', url: '#projects', cName: 'nav-links' },
-  { title: 'Contact', url: '#contact', cName: 'nav-links' },
-];
